5000d10ba4595a185f88dc3540ca0760d59180f5
chris2in <lyh2in@gmail.com>
2022-08-12 23:34:53 -0500

    does not think swipe will really work postgone

--- tictactoe/swipeTest.py
+++ tictactoe/swipeTest.py
@@ -1,4 +1,22 @@
-#   generate a overall swipe over the board to updates
-#   instead of checkAvi function that checks every piece in a board that O(n^4)
-#   this will generate swipe in horizontal or vertical
-#   
+# #   generate a overall swipe over the board to updates
+# #   instead of checkAvi function that checks every piece in a board that O(n^4)
+# #   this will generate swipe in horizontal or vertical
+# a = [[1,2,2,2,2,2,2,2,1],
+#      [1,0,1,0,1,0,0,1,0],
+#      [2,0,1,0,0,2,0,0,0],
+#      [0,0,1,2,0,0,2,2,1],
+#      [0,0,0,2,2,1,0,1,0]
+#      ]
+
+# for row in range(len(a)):
+#     # for col in range(len(a[row])):
+#     # if(0  in a[row] or 3 in a[row] or 4 in a[row] or 5 in a[row]):
+#     #     if(3 )
+#     currentRow = a[row]
+#     if(3 in currentRow or 4 in currentRow or 5 in currentRow):
+#         if(3 in currentRow):
+
+#     if(0 in currentRow):
+    
+    
+    
