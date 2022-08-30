e8ae2f53d409a968d84571fa8a01e05b82928ae8
chris2in <lyh2in@gmail.com>
2022-08-30 10:47:29 -0500

    right left up flip confimed

--- tictactoe/Flip.py
+++ tictactoe/Flip.py
@@ -8,14 +8,14 @@ a= [
 
 b = [
     [0,1,1,1,2],
-    [2,1,0,2,1],
+    [1,2,0,2,1],
     [1,2,2,2,0],
     [0,1,1,1,0],
     [2,1,2,1,1]
 
 
 ]
-
+DEBUGTEST = True
 
 def flip(board,row,col,piece):
     #takes in the 2d array board, x, and y
@@ -26,28 +26,73 @@ def flip(board,row,col,piece):
     widthMAX  = len(board[0])
     heightMAX = len(board)-1
 
-    anchor = piece
+    # anchor = piece
+    board[row][col]= piece
     
     #   checking rightward
     if(col < widthMAX-1):
         # nextRight = board[row][col]
-        rightPivot = board[row][col+1]
-        if(rightPivot > 0 and rightPivot < 3 ):
+        # pivot = board[ row][col]
+        # print(pivot)\
+        directionScore = 0
+        nextRight = board[row][col+1]
+        if(nextRight>0 and nextRight<3 and nextRight != piece):
             for index in range(col+1,widthMAX):
-                nextRight = board[row][index]
-                if(nextRight > 2 or nextRight < 1):
+                if(board[row][index] == piece):
+                    print("found it, break")
                     break
-                if(nextRight != rightPivot):
-                    # if()
-                    #   continue here
+                else:
+                    directionScore +=1
+            for index in range(1,directionScore+1):
+                board[row][col+index] = piece
+                print("FLIP")
+        score += directionScore
+        if(DEBUGTEST):
+            print("right has {} flip".format(directionScore))
 
+    #   checking leftward
+    if(col >= 0):
+        nextLeft = board[row][col-1]
+        directionScore = 0
+        if(nextLeft>0 and nextLeft < 3 and nextLeft != piece):
+            for index in range(col-1,0,-1):
+                if(board[row][index] == piece):
+                    break
+                else:
+                    directionScore +=1 
+            for index in range(1, directionScore+1 ):
+                board[row][col-index] = piece
+        score += directionScore
+        if(DEBUGTEST):
+            print("left has {} flip".format(directionScore))
+    
+    #   checking upward
+    if(row < heightMAX):
+        nextDown = board[row+1][col ]
+        directionScore = 0 
+        if(nextDown>0 and nextDown < 3 and nextDown != piece):
+            for index in range(row +1 , heightMAX):
+                if(board[index][col] ==piece):
+                    break
+                else:
+                    directionScore+=1
+            for index in range(1, directionScore+1):
+                board[row+index][col] = piece
+        score += directionScore
+    
+        if(DEBUGTEST):
+            print("down has {} flip".format(directionScore))
+
+            
+
+    print(score)
 
 
 
 
     return score 
 
-flip(b,1,2,2)
+flip(b,1,2,1)
 
 
 
