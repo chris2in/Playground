bac03579327e922e0319633a5cbc08eca7ed8ac4
chris2in <lyh2in@gmail.com>
2023-05-10 17:10:24 -0500

    temp save

--- Othello/OthelloGame.py
+++ Othello/OthelloGame.py
@@ -4,3 +4,24 @@ from tkinter import *
 size_of_board = 800
 numperRowCol = 8 
 
+class Othello():
+
+    def __init__(self):
+        self.window= Tk()
+
+        self.window.title("Othello")
+
+        self.canvas = Canvas(self.windwo, width = size_of_board, height= size_of_board, bg = 'lightgray')
+        self.canvas.pack()
+
+        self.window.bind('<Button-1>',self.click)
+
+        self.Square= []
+
+
+    def convertGridToBoard(self):
+        return 
+
+    def click(self,event):
+
+        boardPosition = self.convertGridToBoard()
