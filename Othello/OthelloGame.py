369f90451a1817f2b620bf87e9ec86bee789baa1
chris2in <lyh2in@gmail.com>
2023-05-17 16:39:28 -0500

    go home save pt

--- Othello/OthelloGame.py
+++ Othello/OthelloGame.py
@@ -2,7 +2,31 @@ from tkinter import *
 
 
 size_of_board = 800
-numperRowCol = 8 
+numPerRowCol = 8 
+class Square():
+    def __init__(self,OthelloObject, x,y,outline = 'dark gray',indicator = 0):  
+        self.x= x
+        self.y = y
+        self.outline =outline
+        self.indicator = 1
+        SquareNum =y*numPerRowCol + x
+        length = size_of_board/numPerRowCol
+        
+        if(self.indicator == 1):
+            self.color = 'black'
+        elif(self.indicator == 2):
+            self.color = 'white'
+        else:
+            self.color = 'gray'
+        OthelloObject.canvas.create_rectangle(SquareNum%numPerRowCol*length,SquareNum//numPerRowCol*length,SquareNum%numPerRowCol*length+length,SquareNum//numPerRowCol*length+length, outline='dark gray')
+    def changeColor(self,newColor):
+        
+        return 
+    
+    def getX(self):
+        return self.x
+    def getY(self):
+        return self.y
 
 class Othello():
 
@@ -11,12 +35,21 @@ class Othello():
 
         self.window.title("Othello")
 
-        self.canvas = Canvas(self.windwo, width = size_of_board, height= size_of_board, bg = 'lightgray')
+        self.canvas = Canvas(self.window, width = size_of_board, height= size_of_board, bg = 'lightgray')
         self.canvas.pack()
 
         self.window.bind('<Button-1>',self.click)
 
-        self.Square= []
+        self.Squares= []
+
+        length = size_of_board/numPerRowCol
+        for i in range(numPerRowCol):
+            self.Squares.append([])
+            for o in range(numPerRowCol):
+                self.Squares[i].append(Square(self,i,o))
+                
+
+
 
 
     def convertGridToBoard(self):
@@ -25,3 +58,11 @@ class Othello():
     def click(self,event):
 
         boardPosition = self.convertGridToBoard()
+        print("//")
+        print(self.Squares[2][3].getX())
+
+
+    def mainloop(self):
+        self.window.mainloop()
+game_instance = Othello()
+game_instance.mainloop()
