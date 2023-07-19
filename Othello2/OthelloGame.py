d8963e210ca382e3384318618537f7b13646ee2d
chris2in <lyh2in@gmail.com>
2023-07-18 20:21:03 -0500

    color changing merge over

--- Othello2/OthelloGame.py
+++ Othello2/OthelloGame.py
@@ -9,6 +9,40 @@ class Squre():
         self.y = y
         self.outline = outline
         self.indicator = indicator
+        length = size_of_board/numPerRowCol
+        if(self.indicator ==1):
+            self.color = 'black'
+        elif(self.indicator ==2):
+            self.color = 'white'
+        else:
+            self.color=  'gray'
+    
+        
+        self.Rectangle = OthelloObject.canvas.create_rectangle(x*length,
+                                                              y*length,
+                                                              (x+1)*length,
+                                                              (y+1)*length,
+                                                              outline='dark gray')
+
+
+
+    #   example of using this method in Othello:
+    #           self.Squres[x][y].changeColor(self,3)
+    #           where self will pass the Othello object here so you can access the canvas' functions to change color 
+    def changeColor (self,OthelloObject,newIndi):
+        color = 'gray'
+        if(newIndi == 1):
+            color = 'black'
+        elif(newIndi ==2):
+            color = 'white'
+        elif(newIndi ==3):
+            color = 'red'
+        elif(newIndi ==4):
+            color = 'blue'
+        elif(newIndi ==5):
+            color = 'light green'
+        OthelloObject.canvas.itemconfig(self.Rectangle,fill = color)
+
 
 class Othello():
     def __init__(self):
@@ -36,8 +70,9 @@ class Othello():
 
 
     def click(self,event):
-        return ([int(event.x//(size_of_board/numPerRowCol)), int(event.y//(size_of_board/numPerRowCol))])
-    
+        result =  ([int(event.x//(size_of_board/numPerRowCol)), int(event.y//(size_of_board/numPerRowCol))])
+        print(result)
+        # self.Squres[result[0]][result[1]].changeColor(self,3)
     def mainloop(self):
         self.window.mainloop()
 
